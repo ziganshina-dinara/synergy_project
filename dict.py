cae37bf7f7435b4f504d1a3f8dd15ee84923f5a3
dinara <ziganshina.do@phystech.edu>
2021-02-12 01:53:00 +0500

    for creating the dict with signatures

--- dict.py
+++ dict.py
@@ -1,16 +1,15 @@
-def __init__(self, df_inf_score_up, df_inf_score_down):
-    self.df_inf_score_up = df_inf_score_up
-    self.df_inf_score_down = df_inf_score_down
-    self.df_inf_score = pd.concat([df_inf_score_up, df_inf_score_down])['inf_score']
-    self.genes = list(pd.concat([df_inf_score_up, df_inf_score_down]).index)
-    dict_inf_score = {}
-    for (gene, inf_score) in zip(self.genes, self.df_inf_score):
-        dict_inf_score[gene] = inf_score
-    self.dict_inf_score = dict_inf_score
+import json
+with open("DATA/CMap/CD_signatures_binary_42809.gmt", "r") as file:
+    out_of_file_with_signatures = file.read()
+    
+dict_signatures = {}
+for i in range(0, len(out_of_file_with_signatures.split('\n'))-1, 2):
+    start = time.time()
+    signature_up_list = out_of_file_with_signatures.split('\n')[i].split('\t')
+    signature_down_list = out_of_file_with_signatures.split('\n')[i+1].split('\t')
+    dict_signatures[signature_up_list[0]] = (signature_up_list[2:], signature_down_list[2:])
+    print("работа с", i, "-ой сигнатурой для словаря за", time.time() - start)
+    
+with open("DATA/CMap/signatures_dict.json", "w") as write_file:
+    json.dump(dict_signatures, write_file)
 
-
-def get_inf_score(self, gene):
-    try:
-        return self.dict_inf_score[gene]
-    except KeyError:
-        return 1
